7ff2bde7bf20430cc457d2ddab94805a9c5029ae
alexispou07-CAD <alexispou07@gmail.com>
2026-07-31 21:18:55 -0400

    Add conditional rendering for CaptureForm component

--- crm-agence/src/main.jsx
+++ crm-agence/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CRM from "./App.jsx";
+import CaptureForm from "./CaptureForm.jsx";
+
+const isCapturePage = window.location.pathname.startsWith("/capture");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CRM />
+    {isCapturePage ? <CaptureForm /> : <CRM />}
   </React.StrictMode>
 );
